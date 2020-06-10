84335aedc3307a5832d22d4074627b2506b0ee72
Taras-SH13 <60879906+Taras-SH13@users.noreply.github.com>
2020-06-10 11:19:10 +0300

    Initial commit

---
+++ src/main/java/domain/model/Adapters.java
@@ -0,0 +1,12 @@
+package domain.model;
+
+public class Adapters {
+    private String name;
+    private String supply;
+    private Double power;
+    private Double voltage;
+    private Double amperage;
+    private String connector;
+    private Long id;
+
+}
---
+++ src/main/java/domain/model/Laptops.java
@@ -0,0 +1,13 @@
+package domain.model;
+
+public class Laptops {
+
+  private String name;
+  private String brand;
+  private String series;
+  private String model;
+  private Long id;
+
+
+
+}
